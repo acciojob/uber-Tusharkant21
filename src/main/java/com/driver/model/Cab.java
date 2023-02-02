a78310f5f7e5d8cfac4f06ad038543adb49cd0ff
Tusharkant21 <64279043+Tusharkant21@users.noreply.github.com>
2023-02-02 16:08:23 +0530

    final changes

---
+++ src/main/java/com/driver/model/Cab.java
@@ -0,0 +1,61 @@
+package com.driver.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+public class Cab {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int perKmRate;
+
+    private boolean available;
+
+    public Cab(int perKmRate, boolean available) {
+        this.perKmRate = perKmRate;
+        this.available = available;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPerKmRate(int perKmRate) {
+        this.perKmRate = perKmRate;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPerKmRate() {
+        return perKmRate;
+    }
+
+    public boolean getAvailable() {
+        return available;
+    }
+
+    public Cab() {
+    }
+
+
+
+    @OneToOne
+    @JoinColumn
+    @JsonIgnore
+    private Driver driver;
+
+    public Driver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Driver driver) {
+        this.driver = driver;
+    }
+}
